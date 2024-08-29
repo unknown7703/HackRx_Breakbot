22600efbf08213643ba3bd4dce467cdac12be48b
Aatreya <padmajaatreya02@gmail.com>
2024-08-29 17:46:21 +0530

    chore:removed unnecessary file chat , renamed ask to chat , change in env for postgres

--- backend/config.py
+++ backend/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     groq_api_key: str
-    openai_api_key: str
     google_api_key: str
     pinecone_api_key: str
     pinecone_index: str
+    postgres: str
 
     model_config = SettingsConfigDict(env_file="../.env")
