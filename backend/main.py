bfe84b33c2c962db964b51f9d0518c5613afa423
Aatreya <padmajaatreya02@gmail.com>
2025-02-17 21:52:51 +0530

    fix: cors update

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    "https://rag-chatbot-dusky.vercel.app/",
+    "https://rag-chatbot-red.vercel.app/",
 ]
 
 app.add_middleware(
